0c37822447298c51bbc185fa1afa90a60edb2d7c
Ewelina Zukowska <ewelina.zukowska26@gmail.com>
2018-11-09 16:29:32 +0000

    add prop types to BurgerIngredient

--- src/components/Burger/BurgerIngredient/index.js
+++ src/components/Burger/BurgerIngredient/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './index.module.css';
 
@@ -34,6 +35,10 @@ const BurgerIngredient = ({ type }) => {
   }
 
   return ingredient;
+};
+
+BurgerIngredient.PropTypes = {
+  type: PropTypes.string.isRequired
 };
 
 export default BurgerIngredient;
