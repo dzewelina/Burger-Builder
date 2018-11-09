131e72fd1859d6bbeac08ee5f875e9c06a3ae602
Ewelina Zukowska <ewelina.zukowska26@gmail.com>
2018-11-09 16:15:33 +0000

    add BurgerBuilder component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import classes from './index.module.css';
+
 const Layout = ({ children }) => (
   <>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{children}</main>
+    <main className={classes.Content}>{children}</main>
   </>
 );
 
